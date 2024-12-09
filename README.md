dd0318203f84f0040ea9f00cb8cc2885c350c9c1
JuanDuran85 <duran85juan@gmail.com>
2024-12-09 16:56:04 -0300

    working with basic java

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # java_basic
 java basic exercise
+
+
+## Stack and Heap Memory
+Java Heap space is used by java runtime to allocate memory to Objects and JRE classes. Whenever we create an object, 
+it’s always created in the Heap space.
+
+Java Stack memory is used for the execution of a thread. They contain method-specific values that are short-lived and 
+references to other objects in the heap that is getting referred from the method. Stack memory is always referenced in 
+LIFO (Last-In-First-Out) order.
+
+## 
---
+++ basic_theory/variables_data_type/DataTypes.java
@@ -0,0 +1,43 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        System.out.printf("Data Types in Java\n");
+        System.out.println("--------------------------------");
+
+        System.out.println("Primitive Data Types");
+        /*
+         * - Primitive Data Types
+         * A primitive data type specifies the type of a variable and the kind of values
+         * it can hold.
+         * There are eight primitive data types in Java: byte, short, int, long, float,
+         * double, char, and boolean.
+         * long longType = 6546435464346463464L; // The letter 'L' is used to denote a
+         * long integer.
+         * System.out.println(longType);
+         *
+         */
+
+        System.out.println("--------------------------------");
+
+        /*
+         * - Non-Primitive Data Types
+         * Non-primitive data types are called reference types because they refer to
+         * objects.
+         *
+         * The main differences between primitive and non-primitive data types are:
+         * - Primitive types in Java are predefined and built into the language, while
+         * non-primitive types are created by the programmer (except for String).
+         * - Non-primitive types can be used to call methods to perform certain
+         * operations, whereas primitive types cannot.
+         * - Primitive types start with a lowercase letter (like int), while
+         * non-primitive types typically starts with an uppercase letter (like String).
+         * - Primitive types always hold a value, whereas non-primitive types can be
+         * null.
+         *
+         * Examples of non-primitive types are Strings, Arrays, Classes etc. You will
+         * learn more about these in a later chapter.
+         *
+         */
+
+    }
+}

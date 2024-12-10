d42a97cfe37c594ebc4c58f60ec7947e0b3f2437
JuanDuran85 <duran85juan@gmail.com>
2024-12-10 18:25:51 -0300

    scanner methods

--- basic_theory/DataIn/DataIn/src/App.java
+++ basic_theory/DataIn/DataIn/src/App.java
@@ -7,13 +7,13 @@ public class App {
     static final Logger logger = Logger.getLogger(App.class.getName());
     static final String DOT_LINES = "---------------------------";
 
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
 
         logger.log(Level.INFO, DOT_LINES);
         logger.log(Level.INFO, "-------- Scanner Methods --------");
         logger.log(Level.INFO, DOT_LINES);
 
-        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
 
         logger.log(Level.INFO, "Enter your name: ");
@@ -28,8 +28,24 @@ public class App {
         // the next new line character in the scanner, or up to the end of the scanner
         // if there are no more new line characters.
 
+        logger.log(Level.INFO, "Enter your height: ");
+        double height = scanner.nextDouble();
+        // The nextLine() method returns a string containing all of the characters up to
+        // the next new line character in the scanner, or up to the end of the scanner
+        // if there are no more new line characters.
+
+        scanner.nextLine(); // we need to consume the newline character
+
+        logger.log(Level.INFO, "Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        logger.log(Level.INFO, DOT_LINES);
+        scanner.close();
+
         logger.log(Level.INFO, "Name: {0}", name);
+        logger.log(Level.INFO, "Last Name: {0}", lastName);
         logger.log(Level.INFO, "Age: {0}", age);
+        logger.log(Level.INFO, "Height: {0}", height);
 
     }
 }

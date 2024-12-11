d6ba5babdda749ad2e854f9b840b4ddf400f1b61
JuanDuran85 <duran85juan@gmail.com>
2024-12-11 12:37:15 -0300

    operators

---
+++ basic_theory/Operators/src/App.java
@@ -0,0 +1,27 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App {
+    
+    static final Logger logger = Logger.getLogger(App.class.getName());
+    static final String DOT_LINES_TH = "---------------------------";
+    
+    public static void main(String[] args)  {
+        
+        logger.log(Level.INFO, DOT_LINES_TH);
+        logger.log(Level.INFO, "---Operators in Java---");
+        logger.log(Level.INFO, DOT_LINES_TH);
+        
+        /*
+         * Arithmetic Operators: +, -, *, /, %
+         * Assignment Operators: =, +=, -=, *=, /=, %=
+         * Comparison Operators: ==, !=, >, <, >=, <=
+         * Logical Operators: &&, ||, !
+         * Unitary Operators: ++, --
+         * ternary operator ?:
+         */
+
+        
+    }
+}

3cd357e29bf8953eef9fa09dc956aea583a4c6e1
JuanDuran85 <duran85juan@gmail.com>
2025-01-09 18:40:19 -0300

    Refactor comments in ListCollections and SetCollections classes for improved readability and clarity

--- BasicTheory/Others/src/collections/ListCollections.java
+++ BasicTheory/Others/src/collections/ListCollections.java
@@ -7,13 +7,17 @@ import java.util.List;
 public class ListCollections {
     /*
      * List Collections:
-     * An interface that extends Collection and represents an ordered collection (also known as a sequence).
+     * An interface that extends Collection and represents an ordered collection
+     * (also known as a sequence).
      * Lists allow duplicate elements and provide positional access to elements.
      * Common implementations include: ArrayList and LinkedList.
      *
-     * ArrayList: this class is a resizable array, which can be found in the java.util package.
-     * The difference between a built-in array and an ArrayList in Java, is that the size of an array cannot be
-     * modified (if you want to add or remove elements to/from an array, you have to create a new one).
+     * ArrayList: this class is a resizable array, which can be found in the
+     * java.util package.
+     * The difference between a built-in array and an ArrayList in Java, is that the
+     * size of an array cannot be
+     * modified (if you want to add or remove elements to/from an array, you have to
+     * create a new one).
      * While elements can be added and removed from an ArrayList whenever you want.
      */
 
@@ -37,20 +41,28 @@ public class ListCollections {
             System.out.println(element);
         }
 
-
         /*
-        * A lambda expression is a short block of code which takes in parameters and returns a value.
-        * Lambda expressions are similar to methods, but they do not need a name, and they can be implemented
-        * right in the body of a method.
-        *
-        * Represent instances of functional interfaces (interfaces with a single abstract method).
-        * They provide a concise way to express instances of single-method interfaces using a block of code.
-        *
-        * Some Important points about lambda expressions:
-        * - The body of a lambda expression can contain zero, one, or more statements.
-        * - When there is a single statement curly brackets are not mandatory and the return type of the anonymous function is the same as that of the body expression.
-        * - When there is more than one statement, then these must be enclosed in curly brackets (a code block) and the return type of the anonymous function is the same as the type of the value returned within the code block, or void if nothing is returned.
-        */
+         * A lambda expression is a short block of code which takes in parameters and
+         * returns a value.
+         * Lambda expressions are similar to methods, but they do not need a name, and
+         * they can be implemented
+         * right in the body of a method.
+         *
+         * Represent instances of functional interfaces (interfaces with a single
+         * abstract method).
+         * They provide a concise way to express instances of single-method interfaces
+         * using a block of code.
+         *
+         * Some Important points about lambda expressions:
+         * - The body of a lambda expression can contain zero, one, or more statements.
+         * - When there is a single statement curly brackets are not mandatory and the
+         * return type of the anonymous function is the same as that of the body
+         * expression.
+         * - When there is more than one statement, then these must be enclosed in curly
+         * brackets (a code block) and the return type of the anonymous function is the
+         * same as the type of the value returned within the code block, or void if
+         * nothing is returned.
+         */
 
         myStringList.forEach(element -> {
             System.out.println("-------------");
--- BasicTheory/Others/src/collections/SetCollections.java
+++ BasicTheory/Others/src/collections/SetCollections.java
@@ -1,18 +1,23 @@
 package collections;
 
-import java.util.TreeSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetCollections {
 
     /*
      * Set Collections:
-     * An interface that extends Collection and represents a collection of unique elements.
-     * A collection that contains no duplicate elements. More formally, sets contain no pair
+     * An interface that extends Collection and represents a collection of unique
+     * elements.
+     * A collection that contains no duplicate elements. More formally, sets contain
+     * no pair
      * of elements e1 and e2 such that e1.equals(e2), and at most one null element.
-     * As implied by its name, this interface models the mathematical set abstraction.
-     * The Set interface places additional stipulations, beyond those inherited from the Collection interface,
-     * on the contracts of all constructors and on the contracts of the add, equals and hashCode methods.
+     * As implied by its name, this interface models the mathematical set
+     * abstraction.
+     * The Set interface places additional stipulations, beyond those inherited from
+     * the Collection interface,
+     * on the contracts of all constructors and on the contracts of the add, equals
+     * and hashCode methods.
      */
     public static void main(String[] args) {
         // Creating a set

8435c8f2e9f2f211f4d3fe3f6898437a13efcc83
JuanDuran85 <duran85juan@gmail.com>
2025-01-10 23:27:04 -0300

    Enhance Snack class with additional properties and methods; add Snacks class to manage a list of snack items

--- SnackMachine/src/snack/Snack.java
+++ SnackMachine/src/snack/Snack.java
@@ -1,23 +1,69 @@
 package SnackMachine.src.snack;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Snack implements Serializable {
     private static int snackCount = 0;
-    private int snackId;
+    private final int snackId;
     private String name;
     private double price;
 
+
     public Snack(){
         this.snackId = ++Snack.snackCount;
-
     }
 
     public Snack (String name, double price) {
+        this(); // calls the default or empty constructor
         this.name = name;
-        rhis,price = price;
+        this.price = price;
     }
 
+    public static int getSnackCount() {
+        return snackCount;
+    }
+
+    public int getSnackId() {
+        return snackId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "snackId=" + snackId +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) { // Indicates whether some other object is "equal to" this one.
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return snackId == snack.snackId && Double.compare(this.price, snack.price) == 0 && Objects.equals(this.name, snack.name);
+    }
+
+    @Override
+    public int hashCode() { // Returns a hash code value for the object. This method is supported for the benefit of hash tables such as those provided by HashMap
+        return Objects.hash(this.snackId, this.name, this.price);
+    }
 
     public static void main(String[] args) {
 
---
+++ SnackMachine/src/snack/Snacks.java
@@ -0,0 +1,31 @@
+package SnackMachine.src.snack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snacks {
+
+    private static final List<Snack> snacks;
+
+    // static block to initialize the snacks list
+    /*
+    * In Java, a static block executes code before the object initialization. A static block is a block of code with a static keyword
+    * 
+    */
+    static {
+        snacks = new ArrayList<>();
+        snacks.add(new Snack("Chips", 1.99));
+        snacks.add(new Snack("Candy", 0.99));
+        snacks.add(new Snack("Soda", 2.49));
+        snacks.add(new Snack("Ice Cream", 3.99));
+        snacks.add(new Snack("Cookies", 1.49));
+        snacks.add(new Snack("Chocolate", 2.99));
+        snacks.add(new Snack("Milk", 1.29));
+        snacks.add(new Snack("Gum", 0.79));
+        snacks.add(new Snack("Pretzels", 1.49));
+        snacks.add(new Snack("Popcorn", 1.99));
+    }
+
+
+
+}

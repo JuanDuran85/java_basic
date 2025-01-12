c8fead5161253aacb70d14a71d6c44dc8138e207
JuanDuran85 <duran85juan@gmail.com>
2025-01-11 23:26:54 -0300

    Rename Snack classes to better reflect their roles in the domain and presentation layers

--- SnackMachineNew/src/presentation/SnackMachine.java
+++ SnackMachineNew/src/presentation/SnackMachine.java
@@ -1,4 +1,7 @@
-package SnackMachineNew.src.snacks;
+package SnackMachineNew.src.presentation;
+
+import SnackMachineNew.src.domain.Snack;
+import SnackMachineNew.src.services.Snacks;
 
 import java.util.ArrayList;
 import java.util.List;
--- SnackMachineNew/src/services/Snacks.java
+++ SnackMachineNew/src/services/Snacks.java
@@ -1,4 +1,6 @@
-package SnackMachineNew.src.snacks;
+package SnackMachineNew.src.services;
+
+import SnackMachineNew.src.domain.Snack;
 
 import java.util.ArrayList;
 import java.util.List;

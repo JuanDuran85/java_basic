3980a43c288a0ed9ccb29c3308833c624bd246e1
JuanDuran85 <duran85juan@gmail.com>
2025-01-11 23:01:30 -0300

    Add ManageAll class for file reading functionality with example text file

---
+++ BasicTheory/FilesManage/src/manage_all/ManageAll.java
@@ -0,0 +1,24 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ManageAll {
+
+    public static void main(String[] args) {
+        System.out.println(" *** Working with Files with NIO***");
+
+        var fileName = "my_file.txt";
+
+        try {
+            List<String> linesFromFile = Files.readAllLines(Paths.get(fileName));
+            System.out.println(" Contents of " + fileName + " : ");
+            linesFromFile.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}

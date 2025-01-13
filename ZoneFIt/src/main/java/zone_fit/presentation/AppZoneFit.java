894acc0e322bb2723ec2f69c99a88df37d9874eb
JuanDuran85 <duran85juan@gmail.com>
2025-01-13 18:28:36 -0300

    Add menu method to AppZoneFit for improved user interaction

--- ZoneFIt/src/main/java/zone_fit/presentation/AppZoneFit.java
+++ ZoneFIt/src/main/java/zone_fit/presentation/AppZoneFit.java
@@ -1,16 +1,18 @@
 package zone_fit.presentation;
 
+import java.util.Scanner;
+
 public class AppZoneFit {
 
     public static void main(String[] args) {
         System.out.println("Welcome to ZoneFit!");
-
-        
-
-
+        menu();
     }
 
-
+    private static void menu() {
+        boolean exit = false;
+        Scanner console = new Scanner(System.in);
+    }
 
 
 

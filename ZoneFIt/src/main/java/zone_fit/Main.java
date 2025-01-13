79e400b894960d53d64115f53b5e8a6f20464218
JuanDuran85 <duran85juan@gmail.com>
2025-01-12 23:10:46 -0300

    Add initial project structure with .gitignore, pom.xml, and main application class

---
+++ ZoneFIt/src/main/java/zone_fit/Main.java
@@ -0,0 +1,7 @@
+package zone_fit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ ZoneFIt/src/main/java/zone_fit/conexion/Conexion.java
@@ -0,0 +1,10 @@
+package zone_fit.conexion;
+
+import java.sql.Connection;
+
+public class Conexion {
+    public static Connection getConexion(){
+        
+        return ;
+    }
+}

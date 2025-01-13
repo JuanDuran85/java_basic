f9249c55413b277b3c141a5cfc191bc85cd5b07f
JuanDuran85 <duran85juan@gmail.com>
2025-01-13 18:22:44 -0300

    Refactor ClientDAO: remove commented-out code, add deleteClient method, and improve client listing

---
+++ ZoneFIt/src/main/java/zone_fit/presentation/AppZoneFit.java
@@ -0,0 +1,17 @@
+package zone_fit.presentation;
+
+public class AppZoneFit {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to ZoneFit!");
+
+        
+
+
+    }
+
+
+
+
+
+}

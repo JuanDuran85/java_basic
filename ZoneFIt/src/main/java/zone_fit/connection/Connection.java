47016b3769949d706fbf91521a7846dae804a7f6
JuanDuran85 <duran85juan@gmail.com>
2025-01-13 11:49:22 -0300

    Refactor database connection: rename Conexion class to Connection and update ClientDAO to use the new class

---
+++ ZoneFIt/src/main/java/zone_fit/connection/Connection.java
@@ -0,0 +1,31 @@
+package zone_fit.connection;
+
+import java.sql.DriverManager;
+
+public class Connection {
+    public static java.sql.Connection getConexion(){
+        java.sql.Connection connection = null;
+        String dbName = "zona_fit_db";
+        String url = "jdbc:mysql://localhost:3306/" + dbName;
+        String userName = "dummy";
+        String password = "dummy";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, userName, password);
+        } catch (Exception e) {
+            System.out.println("Connection Error: " + e.getMessage());
+        }
+        
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        var connection = Connection.getConexion();
+        if (connection != null){
+            System.out.println("Connection success: " + connection);
+        } else {
+            System.out.println("Connection failed");
+        }
+    }
+}

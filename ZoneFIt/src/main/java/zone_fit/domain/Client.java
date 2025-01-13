bf37551068a732649d26a31ffbf3c3b39e5d4f87
JuanDuran85 <duran85juan@gmail.com>
2025-01-13 11:09:14 -0300

    Add Client class to represent client data and manage membership information

---
+++ ZoneFIt/src/main/java/zone_fit/domain/Client.java
@@ -0,0 +1,81 @@
+package zone_fit.domain;
+
+import java.util.Objects;
+
+public class Client {
+    private int id;
+    private String name;
+    private String lastName;
+    private int membershipId;
+
+    public Client(){}
+
+    public Client(int id){
+        this.id = id;
+    }
+
+    public Client(String name, String lastName, int membershipId){
+        this.name = name;
+        this.lastName = lastName;
+        this.membershipId = membershipId;
+    }
+
+    public Client(int id, String name, String lastName, int membershipId){
+        this(name, lastName, membershipId);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(int membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", membershipId=" + membershipId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id == client.id && membershipId == client.membershipId && Objects.equals(name, client.name) && Objects.equals(lastName, client.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, membershipId);
+    }
+}

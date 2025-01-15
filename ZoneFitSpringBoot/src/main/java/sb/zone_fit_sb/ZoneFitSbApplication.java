3a8c6609a6b360c3a1df4e97e51b8b45ae499adc
JuanDuran85 <duran85juan@gmail.com>
2025-01-14 22:07:02 -0300

    Configure application properties and logging: add database connection settings and create logback configuration

--- ZoneFitSpringBoot/src/main/java/sb/zone_fit_sb/ZoneFitSbApplication.java
+++ ZoneFitSpringBoot/src/main/java/sb/zone_fit_sb/ZoneFitSbApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ZoneFitSbApplication {
 
 	public static void main(String[] args) {
+		// Start the Spring Boot application, which will run the application
 		SpringApplication.run(ZoneFitSbApplication.class, args);
 	}
-
 }

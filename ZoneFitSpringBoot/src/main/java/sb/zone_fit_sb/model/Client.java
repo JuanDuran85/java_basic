0a4cc9b018f586de9dd78a3d7a5c3043d74fa99c
JuanDuran85 <duran85juan@gmail.com>
2025-01-14 22:18:14 -0300

    Add Client entity: define model with fields for id, name, lastName, and membershipId

---
+++ ZoneFitSpringBoot/src/main/java/sb/zone_fit_sb/model/Client.java
@@ -0,0 +1,22 @@
+package sb.zone_fit_sb.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String lastName;
+    private Integer membershipId;
+}

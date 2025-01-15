a13dc78fe40583cd5311f5fbe341e92ff1152cb8
JuanDuran85 <duran85juan@gmail.com>
2025-01-14 21:49:34 -0300

    Initialize ZoneFitSpringBoot project with basic structure and configuration files

---
+++ ZoneFitSpringBoot/src/main/java/sb/zone_fit_sb/ZoneFitSbApplication.java
@@ -0,0 +1,13 @@
+package sb.zone_fit_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoneFitSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoneFitSbApplication.class, args);
+	}
+
+}
---
+++ ZoneFitSpringBoot/src/test/java/sb/zone_fit_sb/ZoneFitSbApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.zone_fit_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneFitSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

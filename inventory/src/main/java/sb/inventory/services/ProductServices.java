80aabfd1f133fe73d2345cf6d16ceebff73ac36e
JuanDuran85 <duran85juan@gmail.com>
2025-01-30 12:06:50 -0300

    Refactor ProductServices to integrate IProductRepository; implement product retrieval, addition, and deletion methods

--- inventory/src/main/java/sb/inventory/services/ProductServices.java
+++ inventory/src/main/java/sb/inventory/services/ProductServices.java
@@ -1,27 +1,37 @@
 package sb.inventory.services;
 
+import org.springframework.stereotype.Service;
 import sb.inventory.model.Product;
+import sb.inventory.repository.IProductRepository;
 
 import java.util.List;
 
+@Service
 public class ProductServices implements IProductServices {
+
+    private final IProductRepository productRepository;
+
+    public ProductServices(IProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        return productRepository.findAll();
     }
 
     @Override
     public Product getProductById(Integer id) {
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 
     @Override
     public void addProduct(Product product) {
-
+        productRepository.save(product);
     }
 
     @Override
     public void deleteProductById(Integer id) {
-
+        productRepository.deleteById(id);
     }
 }

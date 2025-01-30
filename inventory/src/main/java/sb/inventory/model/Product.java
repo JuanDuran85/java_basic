e63cd3c674721f5e62f6537f55ecfb828cb64b41
JuanDuran85 <duran85juan@gmail.com>
2025-01-30 11:09:38 -0300

    Initialize inventory application with basic configuration, logging, and product model

---
+++ inventory/src/main/java/sb/inventory/model/Product.java
@@ -0,0 +1,21 @@
+package sb.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer productId;
+
+    String description;
+    Double price;
+    Integer stock;
+}

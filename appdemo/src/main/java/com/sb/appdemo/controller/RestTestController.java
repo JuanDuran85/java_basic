00fe1ec9a6a66d4ebf9640a669dc7b5c04e69545
JuanDuran85 <duran85juan@gmail.com>
2025-02-04 17:49:00 -0300

    Add Spring Boot DevTools and Actuator dependencies; implement new endpoints for workout and fortune

--- appdemo/src/main/java/com/sb/appdemo/controller/RestTestController.java
+++ appdemo/src/main/java/com/sb/appdemo/controller/RestTestController.java
@@ -10,4 +10,16 @@ public class RestTestController {
     public String testOne() {
         return "Hello World";
     }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getFortune() {
+        return "Your fortune: You'll make a great boss!";
+    }
+
 }

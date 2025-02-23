cb558376a456b9c05931652103dabdeaf5644c57
JuanDuran85 <duran85juan@gmail.com>
2025-02-23 18:22:56 -0300

    Add team info endpoint and update fortune message with coach and team names

--- appdemo/src/main/java/com/sb/appdemo/controller/RestTestController.java
+++ appdemo/src/main/java/com/sb/appdemo/controller/RestTestController.java
@@ -1,10 +1,23 @@
 package com.sb.appdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestTestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
+    }
+
 
     @GetMapping("/")
     public String testOne() {
@@ -19,7 +32,6 @@ public class RestTestController {
 
     @GetMapping("/fortune")
     public String getFortune() {
-        return "Your fortune: You'll make a great boss!";
+        return "Your fortune: You'll make a great boss! " + coachName + " " + teamName;
     }
-
 }

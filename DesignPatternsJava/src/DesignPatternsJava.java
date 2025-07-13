664b8d9f954b140389e2fd942f1357fa0b262367
JuanDuran85 <duran85juan@gmail.com>
2025-07-13 18:06:55 -0400

    Enhance output formatting in DesignPatternsJava to improve clarity of strategy pattern demonstration

--- DesignPatternsJava/src/DesignPatternsJava.java
+++ DesignPatternsJava/src/DesignPatternsJava.java
@@ -4,9 +4,15 @@ import strategy_pattern_package.model.Clown;
 import strategy_pattern_package.model.SquereBalloon;
 
 public class DesignPatternsJava {
-    public static void main(String[] args) {
 
-        System.out.println("Strategy Pattern");
+    private static String LINE_SEPARATOR = "----------------------------------------------------------------------";
+
+    public static void main(String[] args) {
+        System.out.println("Design Patterns in Java");
+
+        //------------------------------------------------------------
+        System.out.println(LINE_SEPARATOR);
+        System.out.println(" ------- Strategy Pattern -------");
 
         ScoreBoard scoreBoard = new ScoreBoard();
         System.out.println("Balloon Tap Score");
@@ -20,5 +26,7 @@ public class DesignPatternsJava {
         System.out.println("SquereBalloon Tap Score");
         scoreBoard.algorithmBase = new SquereBalloon();
         scoreBoard.showScore(3, 5);
+        System.out.println(LINE_SEPARATOR);
+        //------------------------------------------------------------
     }
 }
